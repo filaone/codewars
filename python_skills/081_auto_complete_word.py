97b03643ab1a8e43c45f5ab368e8a3f96ec04ddd
wsg <buaa_wsg@163.com>
2016-09-30 14:24:02 +0800

    add 082

--- python_skills/081_auto_complete_word.py
+++ python_skills/081_auto_complete_word.py
@@ -7,18 +7,43 @@ class youCompleteMe():
     def __init__(self,words):
         self.words = list(words)
         c_words = [];
+        self.createDict(self.words)
     def complete_1(self, head):
         self.c_words = []   
         for ele in self.words:
-            print ele
             if re.compile(head).search(ele):
                 self.c_words.append(ele)
         return self.c_words
+    def complete_2(self,head):
+        if self.dicts.has_key(head):
+            return self.dicts[head]
+        else:
+            return "NO Words!"
+            # if not return somthing the out side print will print "None"
 
+    def createDict(self, words):
+        for ele in words:
+            for i in range(len(ele)):
+                if self.dicts.has_key(ele[:i+1]):
+                    self.dicts[ele[:i+1]].append(ele)
+                else:
+                    self.dicts[ele[:i+1]] = []
+                    self.dicts[ele[:i+1]].append(ele)
+        print self.dicts
     words = [];
     c_words = [];
+    dicts = dict("");
 
 word_test = "this is my test cp mk config con mm dd df break bre".split(" ")
 
 completer = youCompleteMe(word_test)
+print completer.complete_1("br")
+print completer.complete_2("br")
+print "3",completer.complete_1("hello")
+print "4",completer.complete_2("hello")
+print "5",completer.complete_1("thi")
+print "6",completer.complete_2("thi")
+print completer.complete_1("c")
+print completer.complete_2("c")
+print completer.complete_1("m")
 print completer.complete_1("br")
---
+++ python_skills/082_determine_valid_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+
+import re
+
+email_re = re.compile(
+r"(^[-!#$%&'*+/=?^_`{}|~0-9A-Z]+(\.[-!#$%&'*+/=?^_`{}|~0-9A-Z]+)*"  # dot-atom
+r'|^"([\001-\010\013\014\016-\037!#-\[\]-\177]|\\[\001-011\013\014\016-\177])*"' # quoted-string
+r')@(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?$', re.IGNORECASE)  # domain
+
+string = "wang@shu.guang"
+print email_re.search(string).group()
