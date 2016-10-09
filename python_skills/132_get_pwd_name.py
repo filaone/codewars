21cf4a9d34925d2e6cf51f021c922be5a6b024d5
wsg <buaa_wsg@163.com>
2016-10-09 16:55:25 +0800

    add python skills 132-135 and correct the README

---
+++ python_skills/132_get_pwd_name.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+
+import os
+print __file__
+print os.path.realpath(__file__)
+print os.path.dirname(os.path.realpath(__file__))
+print "http://stackoverflow.com/questions/5137497/find-current-directory-and-files-directory"
+
+# current working directory
+print os.getcwd()
